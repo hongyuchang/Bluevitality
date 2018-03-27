7c66080020db6ea454e9a6adb5f5da7e752834a2
bluevitality <19450673+bluevitality@users.noreply.github.com>
2018-03-27 10:09:09 +0800

    Update 测试环境打包迁移后修改.sh

--- Linux/日志及监控/Elasticsearch/测试环境打包迁移后修改.sh
+++ Linux/日志及监控/Elasticsearch/测试环境打包迁移后修改.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 #注意！脚本执行之前需要确定本机的内核参数已经调整为ES需要的值
+
 #当前脚本需要与打包的项目在相同的路径下
 
 set -e
 set -x
 
 head_address="本机IP地址"
+es_cluster_name="ES-CLUSTER"
+es_node_name="node1"
+es_network_host="本机IP地址"
+es_transport_tcp_port="port"
+es_discovery_zen_ping_unicast_hosts="[1.1.1.1:19300]"
+
 
 origin_path=$(pwd)
 
@@ -17,9 +24,16 @@ cd elasticsearch/jdk1.8.0_101/bin
 export PATH=$(pwd):$PATH   #不能存在相同的JAVA_HOME变量，此处仅将JAVA_HOME/bin加入PATH中
 echo "PATH=$PATH" >> ~/.bash_profile
 
+
 #修改配置文件：
 cd $origin_path
-sed  -i '1,2d' elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml       #注意!!!!配置文件中还要修改地址相关
+sed -i '1,2d' elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml       #注意!!!!配置文件中还要修改地址相关
+sed -i "s/cluster.name.*/cluster.name: ${es_cluster_name}/" elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml
+sed -i "s/node.name.*/node.name: ${es_node_name}/g" elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml
+sed -i "s/network.host.*/network.host: ${es_network_host}/g" elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml
+sed -i "s/transport.tcp.port.*/transport.tcp.port: ${es_transport_tcp_port}/g" elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml
+sed -i "s/discovery.zen.ping.unicast.hosts.*/discovery.zen.ping.unicast.hosts: ${es_discovery_zen_ping_unicast_hosts}/g" \
+elasticsearch/elasticsearch-5.5.0/config/elasticsearch.yml
 
 #重新创建软连接，避免旧的软连接失效
 rm -rf elasticsearch/head
