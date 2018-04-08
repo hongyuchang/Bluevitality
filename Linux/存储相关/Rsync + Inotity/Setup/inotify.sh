99664c7d874b562d270c80611cdf2a15975d0519
bluevitality <inmoonlight@163.com>
2018-04-08 12:58:34 +0800

    rsync+inotify修正

--- Linux/存储相关/Rsync + Inotity/Setup/inotify.sh
+++ Linux/存储相关/Rsync + Inotity/Setup/inotify.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-# Check if user is root
-if [ $(id -u) != "0" ]; then
-    printf "Error: You must be root to run this script!\n"
-    exit 1
-fi
-
-if [ -s inotify-tools-3.13 ]; then
-    rm -rf inotify-tools-3.13
-fi
-
-printf "\nInotify Install ...\n\n"
 tar zxvf inotify-tools-3.13.tar.gz
 cd inotify-tools-3.13
 ./configure --prefix=/usr/local
-make
-make install
+make && make install
 cd -
-
-if [ ! -f /usr/local/lib/libinotifytools.so ]; then
-    printf "Error: inotify-tools compile install failed!\n"
-    exit 1
-fi
-
-printf "\n===== Inotify tools install Completed! ====\n\n"
-
