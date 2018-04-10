235c7f40895caae0db8d0e1d6640ab91052f27d1
Bluevitality <19450673+Bluevitality@users.noreply.github.com>
2018-04-10 21:22:26 +0800

    Update Ip-module-check-excel.py

--- Linux/脚本/Ip-module-check-excel.py
+++ Linux/脚本/Ip-module-check-excel.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+#---------------------------------------------------------
+#需要先下载处理Excel的python模块：openpyxl
+#本脚本用于将项目EXCEL中特定SHEET页下的：【IP】、【目录】按行为基础，将每行结合日志的20个监控关键字输出为20行到EXCEL中
+#USE:
+#	python demo.py <EXCEL源文件> <第X个SHEET页> <第X列> <第X列>
+#
+#
+#注：可自动略过EXCEL源文件中的空白行
+#---------------------------------------------------------
+
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
+import sys
+import time 
 
 HOSTS=list()
 MODULE=list()
 
 #读取
-rb = load_workbook(filename=u'in.xlsx')
-rs=rb.get_sheet_by_name(rb.sheetnames[2]) 	#第2个sheet
+INPUT_EXCEL=sys.argv[1]
+EXCEL_SHEET=sys.argv[2]
+EXCEL_COLUMN_IP=sys.argv[3]
+EXCEL_COLUMN_MODULE=sys.argv[4]
+
+#获取输入文件及内部的SHEET页
+rb = load_workbook(filename=str(INPUT_EXCEL))
+rs=rb.get_sheet_by_name(rb.sheetnames[int(EXCEL_SHEET)])
 
 #获取所有行
 rows = rs.rows
 
 #迭代所有行的指定列
+START_NUM=1
+START_ROW=2
 for row in rows:
-	line = [col.value for col in row]
-	HOSTS.append(line[1])
-	MODULE.append(line[3])	
+	if START_NUM >= START_ROW:				#跳过EXCEL中第一行标题后再输入
+		line = [col.value for col in row]
+		HOSTS.append(line[int(EXCEL_COLUMN_IP)])
+		MODULE.append(line[int(EXCEL_COLUMN_MODULE)])	
+	START_NUM+=1
 
 #转换后写入
 wb = Workbook()
@@ -49,13 +71,20 @@ CHECK_STR=[
 "BusinessFlowController error"
 ]
 
+RECORD=0
 x=1
 for a in xrange(len(HOSTS)):
 	for b in xrange(len(CHECK_STR)):
-		ws.cell(row=int(x),column=1).value=HOSTS[a]		#根据关键字个数输出N个相同的主机
-		ws.cell(row=int(x),column=2).value=MODULE[a]		#根据关键字个数输出N个相同的模块地址
-		ws.cell(row=int(x),column=3).value=CHECK_STR[b]		#输出关键字
-		x+=1
+		if len(str(HOSTS[a])) >= 5 and len(str(MODULE[a])) >= 5:
+			ws.cell(row=int(x),column=1).value=HOSTS[a]		    #根据关键字个数输出N个相同的主机
+			ws.cell(row=int(x),column=2).value=MODULE[a]		#根据关键字个数输出N个相同的模块地址
+			ws.cell(row=int(x),column=3).value=CHECK_STR[b]		#输出关键字
+			x+=1
 		
 if __name__ == '__main__':
-        wb.save(filename="1.xlsx")
+	OUT_FILENAME=time.strftime("%Y-%m-%d", time.localtime()) 
+    wb.save(filename=str(OUT_FILENAME)+".xlsx")
+	print("--"*30)
+	print("执行成功!... 请查看输出文件: "+str(OUT_FILENAME)+".xlsx")
+	print("共转换输出了 "+str(x-1)+" 行记录")
+
