97f4fd062ffc447c0885eb0392ff17a8505b9a9a
Bluevitality <19450673+Bluevitality@users.noreply.github.com>
2018-04-23 10:33:39 +0800

    Update Rsync触发同步 1.sh

--- Linux/存储相关/Rsync + Inotity/Setup/Rsync触发同步 1.sh
+++ Linux/存储相关/Rsync + Inotity/Setup/Rsync触发同步 1.sh
@@ -3,23 +3,25 @@
 sysctl -w fs.inotify.max_queued_events="99999999"
 sysctl -w fs.inotify.max_user_watches="99999999"
 sysctl -w fs.inotify.max_user_instances="65535"
+sysctl -p 
 
-if [ ! -f /etc/1.pas ];
-  then echo "123456" > /etc/1.pas
-  chmod 600 /etc/1.pas
+if [ ! -f /etc/1.pas ]; then
+    echo "123456" > /etc/1.pas
+    chmod 600 /etc/1.pas
 fi
 
+log=/usr/local/inotify/logs/rsync.log
+
+user=root
 host1=192.168.225.131
 host2=192.168.225.132
 
 src=/home/
 des=/home/
-user=root
-log=/usr/local/inotify/logs/rsync.log
 
-inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f' -e modify,delete,create,attrib ${src} | while read file 
-do
-    echo "sync ${file}"
-    rsync -vzrtopg --delete -e ssh ${src} ${user}@${host1}:${des} && echo "$(date '+%F') @ ${file} Backed ok" >> $log
-    rsync -vzrtopg --delete -e ssh ${src} ${user}@${host2}:${des} && echo "$(date '+%F') @ ${file} Backed ok" >> $log
-done
+inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %e %w%f' -e move,modify,delete,create,attrib ${src} | while read events
+{
+    echo "events"
+    rsync -vzrtopg --delete -e 'ssh -p 22' ${src} ${user}@${host1}:${des} && echo "$(date '+%F') - ${events}" >> $log
+    rsync -vzrtopg --delete -e 'ssh -p 22' ${src} ${user}@${host2}:${des} && echo "$(date '+%F') - ${events}" >> $log
+}
